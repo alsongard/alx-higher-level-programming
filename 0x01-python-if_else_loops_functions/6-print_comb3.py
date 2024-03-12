c823e7239c4a37113862312461dc8dc5f532f608
alsongard <alsongard@gmail.com>
2024-03-12 13:24:51 -0400

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,13 +3,7 @@ x = 0
 while x < 10:
     for y in range(0,10):
         if x== 9 and y == 9:
-            print(f"{x}{y}", end="\n")
+            print("{}{}".format(x,y),  end="\n")
             continue
-        print(f"{x}{y}", end=", " )
+        print("{}{}".format(x,y),  end=", " )
     x+=1
-
-"""
-for i in range(0, 10):
-    for a in range(0,10):
-        print(f"{i}{a}")
-"""
