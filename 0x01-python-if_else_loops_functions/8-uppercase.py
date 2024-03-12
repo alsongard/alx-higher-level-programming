c5a83400b1c7156ff5edf66f734e7bc79329036b
alsongard <alsongard@gmail.com>
2024-03-12 17:09:54 -0400

    task 9 completed and task 8 not completed

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uppercase(str):
+    if "a" < str and a:
+        pass
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    number_str = str(number)
+    return number_str[-1]
