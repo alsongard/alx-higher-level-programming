3165b0b8abfe773aebeb6aeb31dfafcf45c47dec
alsongard <alsongard@gmail.com>
2024-04-27 05:09:47 -0400

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,10 @@
 def uppercase(str):
     for letter in str:
         upper_case_array = list(range(65, 91))
-        if ord(letter) in upper_case_array or ord(letter) == 32:
+        if ord(letter) == 32 or ord(letter) == 44 or ord(letter) in upper_case_array:
             print("{}".format(letter), end="")
         else:
             lower_int = ord(letter)
             upper = lower_int - 32
             print("{}".format(chr(upper)), end="")
+uppercase("Holberton School, 98 battery street")
