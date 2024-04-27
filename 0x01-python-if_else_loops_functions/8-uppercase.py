cb350f144902696cf7dfd2464112ffab29cf4f61
alsongard <alsongard@gmail.com>
2024-04-27 04:50:01 -0400

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        unicode_char = ord(letter)
-        result = unicode_char - 32
-        print("{}", .format(chr(result)), end="")
+        upper_case_array = list(range(65, 91))
+        if ord(letter) in upper_case_array or ord(letter) == 32:
+            print("{}".format(letter), end="")
+        else:
+            lower_int = ord(letter)
+            upper = lower_int - 32
+            print("{}".format(chr(upper)), end="")
