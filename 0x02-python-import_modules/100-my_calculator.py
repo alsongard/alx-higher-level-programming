2b8e12107a1845e7944a19cda6655c76d3786da9
alsongard <alsongard@gmail.com>
2024-05-02 16:10:49 -0400

    task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    operator = ["+", "-", "*", "/"]
+    argv_length = len(sys.argv) - 1
+    if argv_length != 3:
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    elif sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))  
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
+    from hidden_4 import *
+    allfunction = dir()
+    print(allfunction)
