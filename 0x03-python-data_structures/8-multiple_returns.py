3ecab4cf66200936661ec78cf9228054791830cd
alsongard <alsongard@gmail.com>
2024-05-07 15:44:17 -0400

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return 0, "None"
+        return 0, None
     else:
         my_tuple = ()
         sentence_len = len(sentence)
