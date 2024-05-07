c53d2f2f37ccc3cd883da1715fe30c07b4c68aed
alsongard <alsongard@gmail.com>
2024-05-07 17:50:15 -0400

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,8 @@
 def divisible_by_2(my_list=[]):
     divisibility_list = []
     for i in my_list:
-        divisibility_list.append(i % 2 == 0)
-        print(divisibility_list)
+        if i % 2 == 0:
+            divisibility_list.append(True)
+        else:
+            divisibility_list.append(False)
     return divisibility_list
