1706e75cfb45fa1d8b9912d5de182db05b7d22e5
alsongard <alsongard@gmail.com>
2024-05-07 07:54:32 -0400

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
-        return my_list.copy()
+        origin_list = my_list.copy()
+        return origin_list
     else:
         answers = my_list.copy()
         answers[idx] = element
