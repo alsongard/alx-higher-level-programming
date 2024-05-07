359da0b46850ef7a42fee46ac827e3763f491f52
alsongard <alsongard@gmail.com>
2024-05-07 15:37:01 -0400

    pycodestyle task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_num = 0
-    if len(my_list) ==  0:
+    if len(my_list) == 0:
         return None
     else:
         for i in range(len(my_list)):
