1df024da1026d549854f31d2ccacfd1d27e96280
alsongard <alsongard@gmail.com>
2024-05-13 21:11:46 -0400

    task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for item_1 in set_1:
-        for item_2 in set_2:
-            if item_1 == item_2:
-                answer = item_2
-    return answer
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
+    return set_1.intersection(set_2)
